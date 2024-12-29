5ab912328a8054064e4367e2dcfb6a711c8e2e2d
Wathsu4 <wathsalathathsarani15@gmail.com>
2024-12-29 23:04:51 +0530

    Text Vectorizing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const session = require("express-session");
+require("dotenv").config({ path: ".env.local" });
 
 const app = express();
 
